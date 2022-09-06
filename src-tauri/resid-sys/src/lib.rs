e8cb71a9e29c7cbff43eeb107f386ee14756283a
WilfredC64 <wilfred.bos@xs4all.nl>
2022-09-06 20:15:01 +0200

    disabled Clippy warning which cannot be solved without changing reSID code

--- src-tauri/resid-sys/src/lib.rs
+++ src-tauri/resid-sys/src/lib.rs
@@ -1,6 +1,7 @@
 // Copyright (C) 2022 Wilfred Bos
 // Licensed under the GNU GPL v3 license. See the LICENSE file for the terms and conditions.
 
+#![allow(clippy::upper_case_acronyms)]
 use autocxx::prelude::*;
 
 use ffi::reSID::*;
