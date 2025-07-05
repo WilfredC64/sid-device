81a9d00398fc40951a7b9794dd2954ea9f132f0f
WilfredC64 <wilfred.bos@xs4all.nl>
2025-07-05 11:19:36 +0200

    simplified to avoid double iterating devices

--- src-tauri/src/utils/audio.rs
+++ src-tauri/src/utils/audio.rs
@@ -1,32 +1,28 @@
-// Copyright (C) 2022 Wilfred Bos
+// Copyright (C) 2022 - 2025 Wilfred Bos
 // Licensed under the GNU GPL v3 license. See the LICENSE file for the terms and conditions.
 
-use cpal::Device;
 use cpal::traits::{DeviceTrait, HostTrait};
 
 pub fn get_available_audio_output_device_names() -> (Vec<String>, i32) {
     let host = cpal::default_host();
-    let default_device = host.default_output_device().unwrap();
-    let default_device_name = default_device.name().unwrap();
+    let default_device_name = host
+        .default_output_device()
+        .and_then(|d| d.name().ok())
+        .unwrap_or_default();
 
-    let mut default_device = 0_i32;
-    let devices = get_available_audio_output_devices().iter().enumerate().map(|(index, device)| {
-        let device_name = device.name().unwrap();
-        if device_name.eq(&default_device_name) {
-            default_device = index as i32;
-        }
-        device_name
-    }).collect();
-    (devices, default_device)
-}
-
-pub fn get_available_audio_output_devices() -> Vec<Device> {
-    let host = cpal::default_host();
+    let mut device_names = Vec::new();
+    let mut default_index = 0_i32;
 
     if let Ok(devices) = host.output_devices() {
-        devices.collect()
-    } else {
-        vec![host.default_output_device().expect("Failed to find a default output device")]
+        for (i, device) in devices.enumerate() {
+            if let Ok(name) = device.name() {
+                if name == default_device_name {
+                    default_index = i as i32;
+                }
+                device_names.push(name);
+            }
+        }
     }
-}
 
+    (device_names, default_index)
+}
