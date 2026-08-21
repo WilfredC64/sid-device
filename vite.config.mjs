c9cc9ba5a5233f5423eb6c3b3f393344a1f3353f
WilfredC64 <wilfred.bos@xs4all.nl>
2026-08-21 10:22:36 +0200

    fix warning

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,15 +3,15 @@ import { resolve } from 'path';
 import svgLoader from 'vite-svg-loader';
 import vue from '@vitejs/plugin-vue';
 
-const root = resolve(__dirname, 'src');
-const outDir = resolve(__dirname, 'dist');
+const root = resolve(import.meta.dirname, 'src');
+const outDir = resolve(import.meta.dirname, 'dist');
 
 // https://vitejs.dev/config/
 export default defineConfig({
     root,
     resolve:{
         alias:{
-            '@' : resolve(__dirname, './src')
+            '@' : resolve(import.meta.dirname, './src')
         },
     },
     plugins: [vue(), svgLoader()],
